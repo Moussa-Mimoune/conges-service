1b5f065e4f48ae150d6593e1b2b104227e859204
Moussa Mimoune <moussa.ehm@gmail.com>
2026-07-06 15:42:49 +0200

    feat: PROJ-101 ajout endpoint liste des demandes par employe

--- src/main/java/com/entreprise/conges_service/controller/CongeController.java
+++ src/main/java/com/entreprise/conges_service/controller/CongeController.java
@@ -1,11 +1,13 @@
 package com.entreprise.conges_service.controller;
 
 import com.entreprise.conges_service.model.DemandeConge;
+import com.entreprise.conges_service.repository.DemandeCongeRepository;
 import com.entreprise.conges_service.service.CongeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -13,17 +15,26 @@ import java.util.Map;
 public class CongeController {
 
     private final CongeService service;
+    private final DemandeCongeRepository demandeRepository;
 
-    public CongeController(CongeService service) {
+    // Spring injecte automatiquement les deux dependances
+    public CongeController(CongeService service, DemandeCongeRepository demandeRepository) {
         this.service = service;
+        this.demandeRepository = demandeRepository;
     }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping                         // POST /api/demandes
+    @ResponseStatus(HttpStatus.CREATED)  // renvoie 201 en cas de succes
     public DemandeConge soumettre(@RequestBody Map<String, String> body) {
         Long employeId = Long.parseLong(body.get("employeId"));
         LocalDate debut = LocalDate.parse(body.get("dateDebut"));
         LocalDate fin   = LocalDate.parse(body.get("dateFin"));
         return service.soumettre(employeId, debut, fin);
+    }
+
+    @GetMapping("/employe/{employeId}")  // GET /api/demandes/employe/1
+    public List<DemandeConge> listerParEmploye(@PathVariable Long employeId) {
+        // @PathVariable = recupere {employeId} directement depuis l'URL
+        return demandeRepository.findByEmployeId(employeId);
     }
 }
--- src/main/java/com/entreprise/conges_service/repository/DemandeCongeRepository.java
+++ src/main/java/com/entreprise/conges_service/repository/DemandeCongeRepository.java
@@ -3,8 +3,12 @@ package com.entreprise.conges_service.repository;
 import com.entreprise.conges_service.model.DemandeConge;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// Meme principe que EmployeRepository
-// Spring genere automatiquement toutes les operations CRUD pour DemandeConge
+import java.util.List;
+
 public interface DemandeCongeRepository extends JpaRepository<DemandeConge, Long> {
 
+    // Spring Data genere automatiquement la requete SQL depuis le nom de la methode :
+    // findBy + Employe + Id -> SELECT * FROM demande_conge WHERE employe_id = ?
+    // pas besoin d'ecrire une seule ligne de SQL
+    List<DemandeConge> findByEmployeId(Long employeId);
 }
